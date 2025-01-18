3c964d3e059c20e0dbfc0f175d8baf771a2386ce
Regi Shehi <regi_shehi@hotmail.com>
2025-01-18 10:40:42 +0100

    Add packages for infrastructure project

---
+++ src/core/Domain/Entities/School.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities;
+
+public class School
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime EstablishedOn { get; set; }
+}
