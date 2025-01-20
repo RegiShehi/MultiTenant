785df7d6bc7c380244ac8a69e69ee317717fee89
Regi Shehi <regi_shehi@hotmail.com>
2025-01-20 21:16:06 +0100

    Implement tenant service

--- src/Infrastructure/Tenancy/TenantService.cs
+++ src/Infrastructure/Tenancy/TenantService.cs
@@ -1,12 +1,125 @@
 ﻿using Application.Features.Tenancy;
 using Application.Features.Tenancy.Commands;
+using Application.Features.Tenancy.Models;
+using Finbuckle.MultiTenant;
+using Infrastructure.Persistence.DbInitializers;
+using Mapster;
 
 namespace Infrastructure.Tenancy;
 
-internal class TenantService : ITenantService
+internal class TenantService(
+    ApplicationDbInitializer applicationDbInitializer,
+    IMultiTenantStore<AbcTenantInfo> tenantStore)
+    : ITenantService
 {
-    public Task<string> CreateTenantAsync(CreateTenantRequest createTenantRequest)
+    public async Task<string> CreateTenantAsync(CreateTenantRequest request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        // create tenant in database
+        var newTenant = new AbcTenantInfo
+        {
+            Id = request.Identifier,
+            Name = request.Name,
+            ConnectionString = request.ConnectionString,
+            AdminEmail = request.AdminEmail,
+            ValidUpTo = request.ValidUpTo,
+            IsActive = request.IsActive
+        };
+
+        await tenantStore.TryAddAsync(newTenant);
+
+        // initialize tenant with users, users roles, roles and permissions
+        try
+        {
+            await applicationDbInitializer.InitializeDatabaseAsync(cancellationToken);
+        }
+        catch (Exception)
+        {
+            await tenantStore.TryRemoveAsync(request.Identifier);
+            throw;
+        }
+
+        return newTenant.Id;
+    }
+
+    public async Task<string> ActivateAsync(string id)
+    {
+        var tenant = await tenantStore.TryGetByIdentifierAsync(id);
+
+        if (tenant is null)
+            throw new ArgumentException($"Tenant with {id} could not be found");
+
+        tenant.IsActive = true;
+
+        var updateSuccess = await tenantStore.TryUpdateAsync(tenant);
+
+        if (!updateSuccess || tenant.Id is null)
+            throw new InvalidOperationException($"Failed to update tenant with ID '{id}'.");
+
+        return tenant.Id;
+    }
+
+    public async Task<string> DeactivateAsync(string id)
+    {
+        var tenant = await tenantStore.TryGetByIdentifierAsync(id);
+
+        if (tenant is null)
+            throw new ArgumentException($"Tenant with {id} could not be found");
+
+        tenant.IsActive = false;
+
+        var updateSuccess = await tenantStore.TryUpdateAsync(tenant);
+
+        if (!updateSuccess || tenant.Id is null)
+            throw new InvalidOperationException($"Failed to update tenant with ID '{id}'.");
+
+        return tenant.Id;
+    }
+
+    public async Task<string> UpdateSubscriptionAsync(string id, DateTime newExpiryDate)
+    {
+        var tenant = await tenantStore.TryGetByIdentifierAsync(id);
+
+        if (tenant is null)
+            throw new ArgumentException($"Tenant with {id} could not be found");
+
+        tenant.ValidUpTo = newExpiryDate;
+
+        var updateSuccess = await tenantStore.TryUpdateAsync(tenant);
+
+        if (!updateSuccess || tenant.Id is null)
+            throw new InvalidOperationException($"Failed to update tenant with ID '{id}'.");
+
+        return tenant.Id;
+    }
+
+    public async Task<List<TenantDto>> GetTenantsAsync()
+    {
+        var tenant = await tenantStore.GetAllAsync();
+
+        return tenant.Adapt<List<TenantDto>>();
+    }
+
+    public async Task<TenantDto> GetTenantByIdAsync(string id)
+    {
+        var tenant = await tenantStore.TryGetByIdentifierAsync(id);
+
+        if (tenant is null)
+            throw new ArgumentException($"Tenant with {id} could not be found");
+
+        #region Manual mapping - option 1
+
+        // return new TenantDto
+        // {
+        //     Id = tenant.Id,
+        //     Name = tenant.Name,
+        //     ConnectionString = tenant.ConnectionString,
+        //     AdminEmail = tenant.AdminEmail,
+        //     ValidUpTo = tenant.ValidUpTo,
+        //     IsActive = tenant.IsActive
+        // };
+
+        #endregion
+
+        return tenant.Adapt<TenantDto>();
     }
 }
--- src/core/Application/Features/Tenancy/ITenantService.cs
+++ src/core/Application/Features/Tenancy/ITenantService.cs
@@ -1,8 +1,14 @@
 ﻿using Application.Features.Tenancy.Commands;
+using Application.Features.Tenancy.Models;
 
 namespace Application.Features.Tenancy;
 
 public interface ITenantService
 {
-    Task<string> CreateTenantAsync(CreateTenantRequest createTenantRequest);
+    Task<string> CreateTenantAsync(CreateTenantRequest request, CancellationToken cancellationToken);
+    Task<string> ActivateAsync(string id);
+    Task<string> DeactivateAsync(string id);
+    Task<string> UpdateSubscriptionAsync(string id, DateTime newExpiryDate);
+    Task<List<TenantDto>> GetTenantsAsync();
+    Task<TenantDto> GetTenantByIdAsync(string id);
 }
