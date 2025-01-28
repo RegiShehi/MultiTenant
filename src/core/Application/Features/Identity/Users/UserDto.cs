d0e2a94cb50ef2e363519f715b674ddcc42eb0a2
Regi Shehi <regi_shehi@hotmail.com>
2025-01-28 22:51:16 +0100

    Add IUserService contract

---
+++ src/core/Application/Features/Identity/Users/UserDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Features.Identity.Users;
+
+public class UserDto
+{
+    public required string Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public required string Email { get; set; }
+
+    public required string UserName { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public bool IsActive { get; set; }
+}
---
+++ src/core/Application/Features/Identity/Users/UserRoleDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.Features.Identity.Users;
+
+public class UserRoleDto
+{
+    public required string RoleId { get; set; }
+
+    public required string Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public bool IsAssigned { get; set; }
+}
+
+public class UserRolesRequest
+{
+    private readonly List<UserRoleDto> userRoles = [];
+
+    public IReadOnlyCollection<UserRoleDto> UserRoles => userRoles.AsReadOnly();
+}
